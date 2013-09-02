06ba5c18482b7d617157d06fe38f8f3c7168653d
Harini Saladi <harini@gurupanguji.com>
2013-09-02 15:04:19 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Startupworkforcebeta::Application.config.secret_key_base = 'cbeba2c9fc21985cc64d70478494d2e29d4a27addb056f1e340161152b2090c7ef00750941b4bc6e4fa848d2dc76e933be544d054563d9d7fb293e49622828c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Startupworkforcebeta::Application.config.session_store :cookie_store, key: '_startupworkforcebeta_session'
